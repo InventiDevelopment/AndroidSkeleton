92a883fe631e10fcd84ed758d6637e7cc1376e46
Lukas Vykuka <lukas.vykuka@gmail.com>
2017-05-05 15:24:17 +0200

    Use kotlin-android-extensions for view injection

--- app/src/main/java/cz/inventi/inventiskeleton/presentation/MainActivity.kt
+++ app/src/main/java/cz/inventi/inventiskeleton/presentation/MainActivity.kt
@@ -1,32 +1,24 @@
 package cz.inventi.inventiskeleton.presentation
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.ViewGroup
-
+import android.support.v7.app.AppCompatActivity
 import com.bluelinelabs.conductor.Conductor
 import com.bluelinelabs.conductor.Router
 import com.bluelinelabs.conductor.RouterTransaction
-
-import butterknife.BindView
-import butterknife.ButterKnife
 import cz.inventi.inventiskeleton.R
 import cz.inventi.inventiskeleton.presentation.list.ListController
+import kotlinx.android.synthetic.main.activity_main.*
 
 open class MainActivity : AppCompatActivity() {
 
     private var router: Router? = null
 
-    @JvmField @BindView(R.id.controller_container)
-    internal var container: ViewGroup? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-        ButterKnife.bind(this)
 
-        router = Conductor.attachRouter(this, container!!, savedInstanceState)
+        router = Conductor.attachRouter(this, controllerContainer, savedInstanceState)
         if (!router!!.hasRootController()) {
             router!!.setRoot(RouterTransaction.with(ListController()))
         }
