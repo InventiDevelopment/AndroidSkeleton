c1ae76ecf64d06574277de3c031277e0d5273c22
Tomas Valenta <semanticer@gmail.com>
2017-06-14 17:57:06 +0200

    Prepare ui for post add screen

--- app/src/main/java/cz/inventi/inventiskeleton/presentation/post/add/PostAddController.kt
+++ app/src/main/java/cz/inventi/inventiskeleton/presentation/post/add/PostAddController.kt
@@ -4,9 +4,12 @@ package cz.inventi.inventiskeleton.presentation.post.add
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import cz.inventi.inventiskeleton.R
 import cz.inventi.inventiskeleton.di.conductorlib.ConductorInjection
 import cz.inventi.inventiskeleton.presentation.common.BaseController
+import cz.inventi.inventiskeleton.presentation.common.bindView
 import javax.inject.Inject
 
 /**
@@ -16,6 +19,10 @@ import javax.inject.Inject
 class PostAddController  : BaseController<PostAddView, PostAddPresenter>(), PostAddView {
 
     @Inject lateinit var postAddPresenter: PostAddPresenter
+
+    internal val titleEdit: EditText by bindView(R.id.post_title_edit)
+    internal val bodyEdit: EditText by bindView(R.id.post_body_edit)
+    internal val doneBtn: Button by bindView(R.id.done_btn)
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup): View {
         ConductorInjection.inject(this)
