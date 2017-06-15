7d1341f9e39d47374dfc4ecbf0d52120d7195365
ecnill <ecnill.j@gmail.com>
2017-06-15 17:26:13 +0200

    Post detail with comments functionality

---
+++ app/src/main/java/cz/inventi/inventiskeleton/data/comment/Comment.kt
@@ -0,0 +1,7 @@
+package cz.inventi.inventiskeleton.data.comment
+
+/**
+ * Created by ecnill on 14-Jun-17.
+ */
+
+data class Comment(val postId: Int, val id: Int, val name: String, val email: String, val body: String)
